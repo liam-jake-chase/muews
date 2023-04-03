7e663a2f82dbdc467e8d11fe77a9b30bb36231b8
Josh Fusillo <josh.fusillo@gmail.com>
2023-04-03 00:38:36 -0400

    Style updates

--- src/components/ResultsPage/Profile/Profile.jsx
+++ src/components/ResultsPage/Profile/Profile.jsx
@@ -1,25 +1,25 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { ArtistContext } from "../../../context/ArtistContext";
 import "./Profile.scss";
 
 export default function Profile(props) {
-  const [discogs, setDiscogs] = useContext(ArtistContext);
+  const [discogs] = useContext(ArtistContext);
   return (
     <>
-    <div>
-      {/* <div className="profile"> */}
-        {/* <div className="profile__bandinfo"> */}
-          <ul className="profile__memberlist">
-            {discogs.map((member) => {
-                return <li className="profile__member">{member.name}</li>;
-              })}
-          </ul>
-        </div>
+      <div className="profile">
+        <div className="profile__header">BIOGRAPHY</div>
         <div className="profile__biography">
           {props.artistInfo.strBiographyEN}
         </div>
-      {/* </div> */}
-      {/* </div> */}
+        <div className="profile__bandinfo">
+          BAND MEMBERS
+          <ul className="profile__memberlist">
+            {discogs.map((member) => {
+              return <li className="profile__member">{member.name}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
