563803475b2afb4a5e9d445c2db8cb7c68deacd4
Josh Fusillo <josh.fusillo@gmail.com>
2023-04-04 15:05:44 -0400

    Finished basic functionality for desktop releases section

--- src/components/ResultsPage/Releases/Releases.jsx
+++ src/components/ResultsPage/Releases/Releases.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Releases.scss";
 import FooterNav from "../FooterNav/FooterNav";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Releases(props) {
+  const [albumID, setAlbumID] = useState(undefined);
+  const [albumImage, setAlbumImage] = useState(undefined);
+  const [albumInfo, setAlbumInfo] = useState (undefined) 
+
+  useEffect(() => {
+    axios
+      .get(`https://theaudiodb.com/api/v1/json/2/album.php?m=${albumID}`)
+      .then((response) => {
+        console.log(response.data.album[0].strAlbumThumb);
+        setAlbumImage(response.data.album[0].strAlbumThumb);
+        setAlbumInfo(response.data.album[0].strDescriptionEN)
+      });
+  });
+
   return (
     <>
       <div className="releases">
@@ -13,14 +29,32 @@ export default function Releases(props) {
         />
         <h1 className="releases__header">RELEASES</h1>
         <div className="releases__container">
-          <div className="releases__flex-one">
-            {props.artistReleases.map((albumTitle) => {
-              return <h2 className="releases__title" key={albumTitle.idAlbum}>{albumTitle.strAlbum}</h2>;
+          <div className="releases__flex-one active">
+            {props.artistReleases.map((albumTitle, index) => {
+              return (
+                <li>
+                  <button
+                    className="releases__title"
+                    onClick={() => setAlbumID(albumTitle.idAlbum)}
+                    key={index}
+                  >
+                    {albumTitle.strAlbum}
+                  </button>
+                </li>
+              );
             })}
           </div>
-          <div className="releases__flex-two"></div>
-         
-          <div className="releases__flex-three"></div>
+          <div className="releases__flex-two">
+            <img
+              className="releases__image-thumb"
+              src={albumImage}
+              alt={albumID}
+            />
+          </div>
+
+          <div className="releases__flex-three">
+            <p className="releases__description">{albumInfo}</p>
+          </div>
         </div>
         <footer className="results__footer-nav">
           <FooterNav />
